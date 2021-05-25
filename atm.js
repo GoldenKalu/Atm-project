c5098110f864acae6d9bb60894e9da5d3becf227
Golden Kalu <goldenlemmy@gmail.com>
2021-05-25 15:01:50 -0700

    5th commit

--- atm.js
+++ atm.js
@@ -4,4 +4,21 @@ function getBalance(amountToupdate){
     return accountBalance + amountToupdate;
 }
 
-console.log(accountBalance + amountToupdate);
+module.exports.balance = accountBalance
+module.exports.getBalance = updateAccountBalance
+
+function atmMachinePin(){
+  let pinEntry = parseInt(prompt("enter your pin number"))
+    checkPin(pinEntry);
+}
+ function deposit(){
+    let depositAmount = parseInt(prompt('how much do you want to deposit?'));
+    accountBalance += depositAmount
+    alert("you have completed your deposit" + depositAmount + viewCurrentbalance )
+
+ }
+
+ function withdrawal(){
+     let withdrawalAmount = parseInt(prompt('how much would you lime to withdraw'));
+     
+ }
